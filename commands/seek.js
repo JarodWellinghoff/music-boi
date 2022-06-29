499e4a7027878f968793fd32c95fd6353165115f
Jarod Wellinghoff <jarodwellinghoff@gmail.com>
2022-06-29 02:44:18 -0500

    seek

---
+++ commands/seek.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+// const { getPlayer, updatePlayer } = require('../update-Player');
+// const { Player } = require('discord-player');
+// const { players } = require('../index');
+// const { players } = require('../events/client/interactionCreate');
+// console.log(players);
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('seek')
+		.setDescription('sdfdfsd'),
+	async execute(interaction) {
+		await interaction.deferReply();
+		const { player } = require('../index');
+		// const player = new Player(interaction.client);
+		// const { players } = require('../events/client/interactionCreate');
+		// console.log(players);
+		// const player = players.get(interaction.guildId);
+		// const player = getPlayer();
+		const queue = player.getQueue(interaction.guildId);
+		if (!queue || !queue.playing) {
+			return void interaction.followUp({ content: '❌ | No music is being played!' });
+		}
+		const currentTrack = queue.current;
+		const success = queue.skip();
+		// updatePlayer(player);
+		return void interaction.followUp({
+			content: success ? `✅ | Skipped **${currentTrack}**!` : '❌ | Something went wrong!',
+		});
+	},
+};
