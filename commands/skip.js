ae77d996fff059650fb1d640bb97a98a4715c44c
skellinghoff <Jarodwellinghoff@gmail.com>
2022-07-08 20:06:59 -0500

    sladkfj

--- commands/skip.js
+++ commands/skip.js
@@ -25,38 +25,63 @@ module.exports = {
 			console.log('user.id', user.id);
 			console.log('interaction.user.id', interaction.user.id);
 			console.log('["✅"].includes(reaction.emoji.name)', ['✅'].includes(reaction.emoji.name));
-			return ['✅'].includes(reaction.emoji.name) && user.id === interaction.user.id;
+			return ['✅'].includes(reaction.emoji.name) && !user.bot;
 		};
 
 
 		if (listener_count >= 1) {
+			listener_count = interaction.member.voice.channel.members.size - 1;
+			majority = Math.ceil(listener_count / 2);
+			let vote = 1;
 
-			const message = await interaction.followUp({
-				content: `skip? [1/${majority}]`,
-			});
-			await message.react('✅');
+			const message = await interaction.followUp(`skip? [${vote}/${majority}]`);
+			message.react('✅');
 			//  && skipper !== interaction.user.id
-			await message.awaitReactions({ max: listener_count, time: 60000, errors: ['time'] })
-				.then(collected => {
-					console.log('Reaction');
-					listener_count = interaction.member.voice.channel.members.size - 1;
-					majority = Math.ceil(listener_count / 2);
-					const reaction = collected.first();
-					const vote = collected.size + 1;
+			const collector = message.createReactionCollector({ filter, max: listener_count, time: 60000, errors: ['time'] });
 
-					if (reaction.emoji.name === '✅') {
-						message.edit(`skip? [${vote}/${majority}]`);
-					}
-					if (vote >= majority) {
-						const success = queue.skip();
-						return message.reply({
-							content: success ? `✅ | Skipped **${currentTrack}**!` : '❌ | Something went wrong!',
-						});
-					}
-				})
-				.catch(() => {
-					message.edit('Voting timed out');
-				});
+			collector.on('collect', (reaction) => {
+				if (reaction.emoji.name === '✅') {
+					vote++;
+					message.edit(`skip? [${vote}/${majority}]`);
+				}
+				if (vote >= majority) {
+					const success = queue.skip();
+					return message.reply({
+						content: success ? `✅ | Skipped **${currentTrack}**!` : '❌ | Something went wrong!',
+					});
+				}
+			});
+
+			collector.on('remove', (reaction) => {
+				if (reaction.emoji.name === '✅') {
+					vote--;
+					message.edit(`skip? [${vote}/${majority}]`);
+				}
+			});
+
+			collector.on('end', () => {
+				message.edit('Voting timed out');
+			});
+			// .then(collected => {
+			// 	console.log('Reaction');
+			// 	listener_count = interaction.member.voice.channel.members.size - 1;
+			// 	majority = Math.ceil(listener_count / 2);
+			// 	const reaction = collected.first();
+			// 	const vote = collected.size + 1;
+
+			// 	if (reaction.emoji.name === '✅') {
+			// 		message.edit(`skip? [${vote}/${majority}]`);
+			// 	}
+			// 	if (vote >= majority) {
+			// 		const success = queue.skip();
+			// 		return message.reply({
+			// 			content: success ? `✅ | Skipped **${currentTrack}**!` : '❌ | Something went wrong!',
+			// 		});
+			// 	}
+			// })
+			// .catch(() => {
+			// 	message.edit('Voting timed out');
+			// });
 		}
 		else {
 			const success = queue.skip();
