620fef5bd2293799dc1e0f3df98cd718180d0eb9
skellinghoff <Jarodwellinghoff@gmail.com>
2022-07-11 18:52:01 -0500

    asdf

--- commands/bitcrush.js
+++ commands/bitcrush.js
@@ -1,5 +1,5 @@
 const { AudioFilters } = require("discord-player");
-AudioFilters.define("bitcrush", "acrusher=mix=1:bits=16:aa=0:samples=50");
+AudioFilters.define("bitcrush", "acrusher=mix=1:bits=16:aa=0.5:samples=50");
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
