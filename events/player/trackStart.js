d0f597258e2a062f2290910395a5445ceec11e2d
skellinghoff <Jarodwellinghoff@gmail.com>
2022-07-14 00:31:08 -0500

    embed

--- events/player/trackStart.js
+++ events/player/trackStart.js
@@ -1,12 +1,20 @@
 /* eslint-disable max-len */
+const {MessageEmbed} = require('discord.js');
+
 module.exports = {
   name: 'trackStart',
   execute(queue, track) {
+    const embed = new MessageEmbed()
+        .setColor('GREEN')
+        .setAuthor({
+          name: `${track.requestedBy.username}`,
+          iconURL: `${track.requestedBy.avatarURL()}`,
+        });
     console.log(
         `🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`,
     );
-    queue.metadata.send(
-        `🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`,
-    );
+    queue.metadata.send({
+      embeds: [embed],
+    });
   },
 };
