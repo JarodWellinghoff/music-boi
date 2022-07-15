c689708a8b097fb4ca881d0614f4e31139309861
skellinghoff <Jarodwellinghoff@gmail.com>
2022-07-15 02:20:56 -0500

    asdf

--- commands/play.js
+++ commands/play.js
@@ -22,7 +22,7 @@ module.exports = {
     const searchResult = await player
         .search(query, {
           requestedBy: interaction.user,
-          searchEngine: QueryResolver.resolve(query),
+          searchEngine: QueryType.AUTO,
         })
         .catch((err) => {
           console.error(err);
