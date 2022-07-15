78c2d300e75dd8d952f619044e4713164ebe83b0
skellinghoff <Jarodwellinghoff@gmail.com>
2022-07-15 02:25:46 -0500

    error

--- events/player/error.js
+++ events/player/error.js
@@ -3,7 +3,7 @@ module.exports = {
   name: 'error',
   execute(queue, error) {
     console.log(
-        `[${queue.guild.name}] Error emitted from the queue: ${error.message}`,
+        `[${queue.guild.name}] Error emitted from the queue: ${error.toJSON()}`,
     );
   },
 };
